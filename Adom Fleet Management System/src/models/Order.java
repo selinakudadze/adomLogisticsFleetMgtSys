45e814b91270d1104496d8f9a01f5f1a6acdd170
Elliot Datsomor <datsomorelliot56@gmail.com>
2025-07-13 15:16:16 +0000

    Refactor Order class to include origin and destination coordinates; add InsertionSortByMileage and MergeSortByDriversName classes for sorting vehicles

--- Adom Fleet Management System/src/models/Order.java
+++ Adom Fleet Management System/src/models/Order.java
@@ -8,83 +8,80 @@ public class Order {
     private String clientName;
     private String assignedDriver; 
     private String origin;
+    private double originLatitude;
+    private double originLongitude;
     private String destination;
+    private double destinationLatitude;
+    private double destinationLongitude;
     private String deliveryStatus;
     private LocalDateTime scheduledDateTime;
     private LocalDateTime pickupTime;
     private LocalDateTime eta;
 
     
-    public Order(int orderId, String clientName, String origin, String destination, LocalDateTime scheduledDateTime) {
+    public Order(int orderId, String clientName, String origin, double originLat, double originLong,
+                 String destination, double destinationLat, double destinationLong,
+                 LocalDateTime scheduledDateTime) {
         this.orderId = orderId;
         this.clientName = clientName;
         this.origin = origin;
+        this.originLatitude = originLat;
+        this.originLongitude = originLong;
         this.destination = destination;
+        this.destinationLatitude = destinationLat;
+        this.destinationLongitude = destinationLong;
         this.scheduledDateTime = scheduledDateTime;
         this.deliveryStatus = "Scheduled"; 
     }
 
     
-    public int getOrderId() {
-        return orderId;
-    }
+    public int getOrderId() { return orderId; }
 
-    public String getClientName() {
-        return clientName;
-    }
+    public String getClientName() { return clientName; }
 
-    public String getAssignedDriver() {
-        return assignedDriver;
-    }
+    public String getAssignedDriver() { return assignedDriver; }
 
     public void setAssignedDriver(String assignedDriver) {
         this.assignedDriver = assignedDriver;
     }
 
-    public String getOrigin() {
-        return origin;
-    }
+    public String getOrigin() { return origin; }
 
-    public String getDestination() {
-        return destination;
-    }
+    public double getOriginLatitude() { return originLatitude; }
 
-    public String getDeliveryStatus() {
-        return deliveryStatus;
-    }
+    public double getOriginLongitude() { return originLongitude; }
+
+    public String getDestination() { return destination; }
+
+    public double getDestinationLatitude() { return destinationLatitude; }
+
+    public double getDestinationLongitude() { return destinationLongitude; }
+
+    public String getDeliveryStatus() { return deliveryStatus; }
 
     public void setDeliveryStatus(String deliveryStatus) {
         this.deliveryStatus = deliveryStatus;
     }
 
-    public LocalDateTime getScheduledDateTime() {
-        return scheduledDateTime;
-    }
+    public LocalDateTime getScheduledDateTime() { return scheduledDateTime; }
 
-    public LocalDateTime getPickupTime() {
-        return pickupTime;
-    }
+    public LocalDateTime getPickupTime() { return pickupTime; }
 
     public void setPickupTime(LocalDateTime pickupTime) {
         this.pickupTime = pickupTime;
     }
 
-    public LocalDateTime getEta() {
-        return eta;
-    }
+    public LocalDateTime getEta() { return eta; }
 
     public void setEta(LocalDateTime eta) {
         this.eta = eta;
     }
 
     public Duration getWaitTime() {
-        if (pickupTime == null) {
-            return Duration.ZERO;
-        }
+        if (pickupTime == null) return Duration.ZERO;
         return Duration.between(scheduledDateTime, pickupTime);
     }
 
-    
     public void assignToDriver(String driverName) {
         this.assignedDriver = driverName;
     }
@@ -93,14 +90,15 @@ public class Order {
         this.deliveryStatus = newStatus;
     }
 
-    
     @Override
     public String toString() {
         return "Order ID: " + orderId +
                "\nClient: " + clientName +
                "\nDriver: " + (assignedDriver != null ? assignedDriver : "Unassigned") +
                "\nOrigin: " + origin +
+               " (Lat: " + originLatitude + ", Long: " + originLongitude + ")" +
                "\nDestination: " + destination +
+               " (Lat: " + destinationLatitude + ", Long: " + destinationLongitude + ")" +
                "\nStatus: " + deliveryStatus +
                "\nScheduled: " + scheduledDateTime +
                "\nPickup Time: " + (pickupTime != null ? pickupTime : "N/A") +
