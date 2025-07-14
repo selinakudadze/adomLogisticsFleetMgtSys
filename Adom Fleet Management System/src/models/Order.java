44d76314aa37a584c52ea102572e878776ee84f6
USER <jeffkalbeni@gmail.com>
2025-07-14 15:27:47 +0000

    Order assignment and rerouting issue resolved

--- Adom Fleet Management System/src/models/Order.java
+++ Adom Fleet Management System/src/models/Order.java
@@ -5,29 +5,76 @@ import java.time.Duration;
 
 public class Order {
 
+    private int orderId;
+    private String clientName;
+    private String assignedDriver;
     public enum DeliveryStatus{
         IN_TRANSIT,
         DELIVERED,
         REROUTED
     }
-    private int orderId;
-    private String clientName;
-    private String assignedDriver; 
+
     private String origin;
     private String destination;
     private String deliveryStatus;
     private LocalDateTime scheduledDateTime;
     private LocalDateTime pickupTime;
     private LocalDateTime eta;
+    private double originLatitude;
+    private double originLongitude;
+    private double destinationLatitude;
+    private double destinationLongitude;
+    private double currentLatitude;
+    private double currentLongitude;
+
+//    public enum DeliveryStatus{
+//        IN_TRANSIT,
+//        DELIVERED,
+//        REROUTED
+//    }
+//    private int orderId;
+//    private String clientName;
+//    private String assignedDriver;
+//    private String origin;
+//    private String destination;
+//    private String deliveryStatus;
+//    private LocalDateTime scheduledDateTime;
+//    private LocalDateTime pickupTime;
+//    private LocalDateTime eta;
 
     
-    public Order(int orderId, String clientName, String origin, String destination, LocalDateTime scheduledDateTime) {
+//    public Order(int orderId, String clientName, String origin, String destination, LocalDateTime scheduledDateTime) {
+//        this.orderId = orderId;
+//        this.clientName = clientName;
+//        this.origin = origin;
+//        this.destination = destination;
+//        this.scheduledDateTime = scheduledDateTime;
+//        this.deliveryStatus = "Scheduled";
+//    }
+
+    public Order(
+            int orderId,
+            String clientName,
+            String origin,
+            String destination,
+            LocalDateTime scheduledDateTime,
+            double originLatitude,
+            double originLongitude,
+            double destinationLatitude,
+            double destinationLongitude
+    ) {
         this.orderId = orderId;
         this.clientName = clientName;
         this.origin = origin;
         this.destination = destination;
         this.scheduledDateTime = scheduledDateTime;
-        this.deliveryStatus = "Scheduled"; 
+        this.deliveryStatus = "Scheduled";
+        this.originLatitude = originLatitude;
+        this.originLongitude = originLongitude;
+        this.destinationLatitude = destinationLatitude;
+        this.destinationLongitude = destinationLongitude;
+        this.currentLatitude = originLatitude;
+        this.currentLongitude = originLongitude;
     }
 
     
@@ -55,7 +102,7 @@ public class Order {
         return destination;
     }
 
-    public String getDeliveryStatus(){return this.deliveryStatus.toString();}
+    public String getDeliveryStatus() { return deliveryStatus; }
 
     public void setDeliveryStatus(String deliveryStatus) {
         this.deliveryStatus = deliveryStatus;
@@ -88,14 +135,40 @@ public class Order {
         return Duration.between(scheduledDateTime, pickupTime);
     }
 
+    public double getOriginLatitude() {
+        return originLatitude;
+    }
+
+    public double getOriginLongitude() {
+        return originLongitude;
+    }
+
+    public double getDestinationLatitude() {
+        return destinationLatitude;
+    }
+
+    public double getDestinationLongitude() {
+        return destinationLongitude;
+    }
+
+    public double getCurrentLatitude() {
+        return currentLatitude;
+    }
+
+    public double getCurrentLongitude() {
+        return currentLongitude;
+    }
+
+    public void updateCurrentLocation(double latitude, double longitude) {
+        this.currentLatitude = latitude;
+        this.currentLongitude = longitude;
+    }
     
     public void assignToDriver(String driverName) {
         this.assignedDriver = driverName;
     }
 
-    public void updateDeliveryStatus(DeliveryStatus newStatus) {
-        this.deliveryStatus = newStatus.toString();
-    }
+    public void updateDeliveryStatus(String newStatus) { this.deliveryStatus = newStatus; }
 
 
     
@@ -104,8 +177,9 @@ public class Order {
         return "Order ID: " + orderId +
                "\nClient: " + clientName +
                "\nDriver: " + (assignedDriver != null ? assignedDriver : "Unassigned") +
-               "\nOrigin: " + origin +
-               "\nDestination: " + destination +
+               "\nOrigin: " + origin + " (" + originLatitude + ", " + originLongitude + ")" +
+               "\nDestination: " + destination + " (" + destinationLatitude + ", " + destinationLongitude + ")" +
+                "\nCurrent Location: (" + currentLatitude + ", " + currentLongitude + ")" +
                "\nStatus: " + deliveryStatus +
                "\nScheduled: " + scheduledDateTime +
                "\nPickup Time: " + (pickupTime != null ? pickupTime : "N/A") +
